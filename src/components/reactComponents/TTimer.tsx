064b0af8fab0e1b0f0b1b4fddb689e16acd95b4a
patrickpiedad <patrickpiedad@gmail.com>
2025-07-11 18:53:30 +0200

    created initial states for ttimer

--- src/components/reactComponents/TTimer.tsx
+++ src/components/reactComponents/TTimer.tsx
@@ -1,3 +1,25 @@
+type TTimerPhase = 'chill' | 'push'
+type TTimerMode = 'cycles' | 'total-time'
+
+type TTimerSettings = {
+  chillTime: number
+  chillTimeSeconds: number
+  pushTime: number
+  pushTimeSeconds: number
+  cycles: number
+  totalTime: number
+  totalTimeSeconds: number
+  mode: TTimerMode
+}
+
 export default function () {
+  const [time, setTime] = useState<number>(0)
+  const [inputMinutes, setInputMinutes] = useState<number>(5)
+  const [inputSeconds, setInputSeconds] = useState<number>(0)
+
+  const [tTimerPhase, setTTimerPhase] = useState<tTimerPhase>('chill')
+  const [tTimerTotalElapsed, setTTimerTotalElapsed] = useState<number>(0)
+  const [showTTimerSettings, setShowTTimerSettings] = useState<boolean>(false)
+
   return <main>T-Timer App</main>
 }
